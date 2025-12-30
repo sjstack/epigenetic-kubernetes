db14e9efc67c9ec1606cafe555edcbb3eaf712bd
Scott Stackley <scott.stackley@gmail.com>
2025-12-30 20:27:28 +0000

    Add a StatefulSet manifest for regular organism, update controller logic

--- controller/controller.py
+++ controller/controller.py
@@ -1,8 +1,11 @@
+import os
 import time
 from kubernetes import client, config, watch
 
-NAMESPACE = "chaos-genome"
-STRESS_THRESHOLD = 1 # number of restarts to trigger epigenetic change
+NAMESPACE = os.getenv("NAMESPACE", "chaos-genome")
+ORGANISM_TYPE = os.getenv("ORGANISM_TYPE", "StatefulSet")
+# number of restarts to trigger epigenetic change
+STRESS_THRESHOLD = int(os.getenv("STRESS_THRESHOLD", "1"))
 
 def get_k8s_client():
     # Load K8s config (In-cluster if deployed, local if testing)
@@ -40,7 +43,11 @@ def methylate_organism(apps_v1, pod_name, current_level):
            }
        }
    }
-   apps_v1.patch_namespaced_deployment("clonal-organism", NAMESPACE, body)
+
+   if ORGANISM_TYPE.lower() == "deployment":
+       apps_v1.patch_namespaced_deployment("clonal-organism", NAMESPACE, body)
+   elif ORGANISM_TYPE.lower() == "statefulset":
+       apps_v1.patch_namespaced_stateful_set("organism", NAMESPACE, body)
 
 def monitor_cells(v1, apps_v1):
     print("Watching for environmental stress (restarts)...")
@@ -50,13 +57,21 @@ def monitor_cells(v1, apps_v1):
         status = pod.status
 
         if event['type'] == "DELETED":
-            deployment = apps_v1.read_namespaced_deployment("clonal-organism", NAMESPACE)
-            current_mark = deployment.spec.template.metadata.annotations.get(
-                "epigenetic-mark.science/methylation-level",
-                "0"
-            )
-
-            methylate_organism(apps_v1, "Colony", current_mark)
+            if ORGANISM_TYPE.lower == "deployment":
+                deployment = apps_v1.read_namespaced_deployment("clonal-organism", NAMESPACE)
+                if deployment.status.updated_replics == deployment.spec.replicas:
+                    current_mark = deployment.spec.template.metadata.annotations.get(
+                        "epigenetic-mark.science/methylation-level",
+                        "0"
+                    )
+                    methylate_organism(apps_v1, "Colony", current_mark)
+            elif ORGANISM_TYPE.lower == "statefulset":
+                deployment = apps_v1.read_namespaced_deployment("organism", NAMESPACE)
+                current_mark = deployment.spec.template.metadata.annotations.get(
+                    "epigenetic-mark.science/methylation-level",
+                    "0"
+                )
+                methylate_organism(apps_v1, "Colony", current_mark)
 
 def main():
     v1, apps_v1 = get_k8s_client()
