3584d9ed1d4d905e97c0a96664f5a66de3d7bfa6
Scott Stackley <scott.stackley@gmail.com>
2026-01-03 00:18:35 +0000

    Start work on turning controller functionality into a class, just a place holder for now.

---
+++ controller/epigenetic_controller.py
@@ -0,0 +1,19 @@
+
+class EpigeneticController:
+    def __init__(self):
+        print("HERE")
+
+    def get_namespace(self):
+        print("THERE")
+
+    # need to figure out a naming convention for all the levels for these operations
+    def update_template(self):
+        print("BAZ")
+        
+    def methylation(self):
+        print("FOO")
+
+    def demethylation(self):
+        print("BAR")
+    #
+    
